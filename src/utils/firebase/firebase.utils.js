4c5ea4ebaa5b21f37bb05d83ae76af29276e266e
Sylph08 <trongvuxuan1998@gmail.com>
2022-10-09 17:59:53 +0700

    Home PC first commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,85 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBa_QF4ILbOr8GAsqXGxduUql4V_K9Du88",
+  authDomain: "crwn-clothing-db-e5047.firebaseapp.com",
+  projectId: "crwn-clothing-db-e5047",
+  storageBucket: "crwn-clothing-db-e5047.appspot.com",
+  messagingSenderId: "87588232149",
+  appId: "1:87588232149:web:5876756e95287a8e8efb5c"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = () => {
+  signOut(auth);
+}
+
+export const onAuthStateChangedListener = (callback) => onAuthStateChanged(auth, callback);// the second parameter is the callback that you want to call every time this auth state changes
+// onAuthStateChanged return a unsubcribe function to make onAuthStateChanged stop listening to the 'auth' changes
