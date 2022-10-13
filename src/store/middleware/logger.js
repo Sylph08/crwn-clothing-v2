35b67ba238265352323520142673de7e16dcddef
HO.FPT.VN\TrongVX <trongvx@fpt.com.vn>
2022-10-13 17:59:44 +0700

    add redux persist and redux devtool

---
+++ src/store/middleware/logger.js
@@ -0,0 +1,14 @@
+// write our own logger
+export const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) {
+        return next(action);
+    }
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+    //can use this because it is synchonosly
+
+    console.log('nextState: ', store.getState());
+}
--- src/store/store.js
+++ src/store/store.js
@@ -2,29 +2,30 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+//import { loggerMiddleware } from "./middleware/logger";
 
-// write own logger
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) {
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
+const persistConfig = {
+    key: 'root', // root mean we want to persist the whole thing
+    storage: storage,
+    blacklist: ['user']    // pass in the reducer in the combineReducers obj we don't want to persist
+};
 
-    next(action);
-    //can use this because it is synchonosly
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-    console.log('nextState: ', store.getState());
-}
-
-const middleWares = [loggerMiddleware];
 // whenever you dispatch an action, before that action hits the reducers, it hits the middleware first.
+//const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); // process.env.NODE_ENV tell us the environment we are in
+// filter(Boolean) filter out all the value having Boolean type and return an array
 
 //const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
     middleware: middleWares,
+    devTools: process.env.NODE_ENV !== 'production', // another choise: development
     preloadedState: undefined
 });
+
+export const persistor = persistStore(store);
