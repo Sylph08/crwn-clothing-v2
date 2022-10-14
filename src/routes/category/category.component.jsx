748946da0eb45c7ed175dd39e471ddd75f2369cb
Sylph08 <trongvuxuan1998@gmail.com>
2022-10-14 16:29:42 +0700

    add redux-thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,12 +3,14 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { Fragment } from 'react/cjs/react.development';
 import ProductCard from '../../components/product-card/product-card.component';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesIsLoading, selectCategoriesMap } from '../../store/categories/category.selector';
 import './category.styles.scss';
 
 const Category = () => {
     const { category } = useParams(); // take the param from URL
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -18,12 +20,18 @@ const Category = () => {
     return (
         <Fragment>
             <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
-                {
-                    products && products.map((item) => <ProductCard key={item.id} product={item} />)
-                    // need to check 'products' because it is a result of async function
-                }
-            </div>
+            {
+                isLoading ? (<Spinner />) :
+                    (
+                        <div className='category-container'>
+                            {
+                                products && products.map((item) => <ProductCard key={item.id} product={item} />)
+                                // need to check 'products' because it is a result of async function
+                            }
+                        </div>
+                    )
+            }
+
         </Fragment>
     );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -5,18 +5,21 @@ import { rootReducer } from "./root-reducer";
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 //import { loggerMiddleware } from "./middleware/logger";
+import thunk from "redux-thunk";
 
 const persistConfig = {
     key: 'root', // root mean we want to persist the whole thing
     storage: storage,
-    blacklist: ['user']    // pass in the reducer in the combineReducers obj we don't want to persist
+    //blacklist: ['user']
+    // pass to [] the reducer in the combineReducers obj we don't want to persist, everything else in combineReducers is persist
+    whitelist: ['cart'] // everything else is blacklist
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // whenever you dispatch an action, before that action hits the reducers, it hits the middleware first.
 //const middleWares = [loggerMiddleware];
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean); // process.env.NODE_ENV tell us the environment we are in
+const middleWares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean); // process.env.NODE_ENV tell us the environment we are in
 // filter(Boolean) filter out all the value having Boolean type and return an array
 
 //const composedEnhancers = compose(applyMiddleware(...middleWares));
